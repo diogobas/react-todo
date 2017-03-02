0de269b4539f1eff90b37044e08c5623f210d306
Diogo Bastos <diogo.bastos@neovation.com>
2017-03-02 09:58:37 -0600

    Add actions for firebase login and logout

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -4,7 +4,7 @@ try {
   // Initialize Firebase
   var config = {
     apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTHO_DOMAIN,
+    authDomain: process.env.AUTH_DOMAIN,
     databaseURL: process.env.DATABASE_URL,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID
@@ -15,5 +15,6 @@ try {
 
 }
 
+export var githubProvider = new firebase.auth.GithubAuthProvider();
 export var firebaseRef = firebase.database().ref();
 export default firebase;
